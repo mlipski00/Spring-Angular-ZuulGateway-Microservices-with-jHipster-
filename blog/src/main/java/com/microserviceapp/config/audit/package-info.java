3ca1a3c0d8c0240984b5172bbe19873d342d501f
mlipski00 <michal.lipski2@gmail.com>
2018-12-03 23:41:05 +0100

    Added new microservice.

---
+++ blog/src/main/java/com/microserviceapp/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.microserviceapp.config.audit;
---
+++ blog/src/main/java/com/microserviceapp/repository/BlogRepository.java
@@ -0,0 +1,16 @@
+package com.microserviceapp.repository;
+
+import com.microserviceapp.domain.Blog;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data MongoDB repository for the Blog entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface BlogRepository extends MongoRepository<Blog, String> {
+
+}
